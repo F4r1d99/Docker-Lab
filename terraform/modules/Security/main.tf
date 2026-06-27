dad484a07fca6cc34a1eb0025c37b7c953d728cb
frdamirul <frdamirul09@gmail.com>
2026-06-27 19:31:06 +0800

    update iam role

--- terraform/modules/Security/main.tf
+++ terraform/modules/Security/main.tf
@@ -30,27 +30,24 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_iam_role" "ssm_role" {
-  name = var.iam_role_name
-
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ssm_role.name
+  role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = var.instance_profile_name
-  role = aws_iam_role.ssm_role.name
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm_role.name
 }
